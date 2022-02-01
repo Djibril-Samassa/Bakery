e81480dca3caf0570a5371b04659b5ad1a10afbf
Djibril Samassa <djibrilsamassa.pro@gmail.com>
2022-02-01 16:22:51 +0100

    bakery commit

---
+++ src/composants/Button.js
@@ -0,0 +1,18 @@
+// Button.jsx
+import React, { Component } from 'react'
+
+class Button extends Component {
+    
+    render() {
+        return (
+            <button
+                onClick={this.props.onClick} 
+                className={this.props.isSelected === true?  "btn btn-primary" : "btn btn-light"}
+                type="button">
+                    {this.props.children}
+            </button>
+        )
+    }
+}
+
+export default Button
---
+++ src/composants/Pay.js
@@ -0,0 +1,14 @@
+// Pay.jsx
+import React, { Component } from 'react'
+
+class Pay extends Component {
+    render() {
+        return (
+            <div>
+                Pay
+            </div>
+        )
+    }
+}
+
+export default Pay
